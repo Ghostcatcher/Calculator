b1023c1712b33148e5cdb30c162497419ad271b1
Ghostcatcher <geoffrey.freno@viacesi.fr>
2016-11-04 09:46:27 +0100

    feature-subtraction

--- main.c
+++ main.c
@@ -5,11 +5,16 @@ int AdditionNombres(int a, int b){
 
     return a+b;
 }
+int SustractiontionNombres(int a, int b){
 
+    return a-b;
+}
 
 int main (){
 
     printf("%d \n",AdditionNombres(1,4));
+    printf("%d \n",SustractiontionNombres(1,4));
+
     return 0;
 
 }
