5b76c91745fe283ac56cb8364760abf5c2dd5f14
Ghostcatcher <geoffrey.freno@viacesi.fr>
2016-11-04 09:25:58 +0100

    addition of 2 int

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
+int AdditionNombres(int a, int b){
+
+    return a+b;
+}
+
+
+int main (){
+
+    printf("%d \n",AdditionNombres(1,4));
+    return 0;
 
 }
